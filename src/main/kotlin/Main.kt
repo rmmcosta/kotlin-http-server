db0058e2732966ace7bdf226a18d25395cf60748
rmmcosta-hibob <ricardo.costa@hibob.io>
2025-04-21 19:04:35 +0100

    feat:answer to connection with 200 ok

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,11 +6,16 @@ fun main() {
 
     // Uncomment this block to pass the first stage
     val serverSocket = ServerSocket(4221)
-    //
+
     // // Since the tester restarts your program quite often, setting SO_REUSEADDR
     // // ensures that we don't run into 'Address already in use' errors
     serverSocket.reuseAddress = true
-    //
-    serverSocket.accept() // Wait for connection from client.
-    println("accepted new connection")
+
+    while (true) {
+        val clientSocket = serverSocket.accept() // Wait for connection from client.
+        println("accepted new connection")
+        val response = "HTTP/1.1 200 OK\r\n\r\n".toByteArray()
+        clientSocket.getOutputStream().write(response)
+        clientSocket.close()
+    }
 }
