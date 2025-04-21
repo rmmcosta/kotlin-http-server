947ed2b9b279cb013fd4f15261bd09944ff87366
rmmcosta-hibob <ricardo.costa@hibob.io>
2025-04-21 18:08:43 +0100

    feat:accepting connections on port 4221

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,12 +5,12 @@ fun main() {
     println("Logs from your program will appear here!")
 
     // Uncomment this block to pass the first stage
-    // var serverSocket = ServerSocket(4221)
+    val serverSocket = ServerSocket(4221)
     //
     // // Since the tester restarts your program quite often, setting SO_REUSEADDR
     // // ensures that we don't run into 'Address already in use' errors
-    // serverSocket.reuseAddress = true
+    serverSocket.reuseAddress = true
     //
-    // serverSocket.accept() // Wait for connection from client.
-    // println("accepted new connection")
+    serverSocket.accept() // Wait for connection from client.
+    println("accepted new connection")
 }
