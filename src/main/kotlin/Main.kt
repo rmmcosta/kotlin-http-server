b565ab5b4178c3715afe904e03062efae51fc5b3
rmmcosta-hibob <ricardo.costa@hibob.io>
2025-04-29 20:43:49 +0100

    chore:print the args

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,6 +3,9 @@ import java.net.ServerSocket;
 fun main(args: Array<String>) {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println("Logs from your program will appear here!")
+    val argsMsg =
+        if (args.isEmpty()) "the args array is empty" else "the args passed to the main: ${args.forEach(::println)}"
+    println(argsMsg)
     val serverPortKey = "server-port:"
     val port: Int =
         args.firstOrNull { it.contains(serverPortKey) }?.removePrefix(serverPortKey)?.toInt() ?: SERVER_PORT
