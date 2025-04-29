48c9999fcde5a10d09a330a918440e1bbeeaa477
rmmcosta-hibob <ricardo.costa@hibob.io>
2025-04-29 20:57:44 +0100

    fix:retrieving the directory from the args

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,12 +4,13 @@ fun main(args: Array<String>) {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println("Logs from your program will appear here!")
     val argsMsg =
-        if (args.isEmpty()) "the args array is empty" else "the args passed to the main: ${args.forEach(::println)}"
+        if (args.isEmpty()) "the args array is empty" else "the args passed to the main: ${args.joinToString(",")}"
     println(argsMsg)
     val serverPortKey = "server-port:"
     val port: Int =
         args.firstOrNull { it.contains(serverPortKey) }?.removePrefix(serverPortKey)?.toInt() ?: SERVER_PORT
-    val defaultDir: String = System.getProperty("user.dir")
+    val directoryArgIndex = args.indexOf("--directory") + 1
+    val defaultDir: String = args.getOrNull(directoryArgIndex) ?: System.getProperty("user.dir")
     println("default dir: $defaultDir")
     MyHttpServer(ServerSocket(port), defaultDir).initServer()
 }
